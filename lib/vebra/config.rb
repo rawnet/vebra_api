5de71af206d7d835435791f46c8f338e031a90ce
Mike Fulcher <mike@plan9design.co.uk>
2012-04-12 15:31:34 +0100

    Default to the rails temp dir

--- lib/vebra/config.rb
+++ lib/vebra/config.rb
@@ -26,7 +26,7 @@ module Vebra
       @@client_password = nil
       @@client_data_feed_id = nil
       @@debug_mode = false
-      @@tmp_dir = nil
+      @@tmp_dir = defined?(Rails) ? Rails.root.join('tmp') : nil
       @@models = {
         :property => { :class => :property, :files_method => :files, :address_method => :address, :rooms_method => :rooms },
         :address  => { :class => :address },
